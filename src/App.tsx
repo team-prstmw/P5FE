1f17cf6289cabd7241c954ef98a7658d7f3cd1c2
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-05-15 23:25:51 +0200

    refactor: cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import './App.css';
 
-import { ReactElement, useState } from 'react';
-import { useQuery } from 'react-query';
-
-import helloRequest from './api/hello';
 import MenuPage from './components/UI/pages/MenuPage';
 
-function ServerMessage() {
-  const { data, isLoading, error } = useQuery('hello', helloRequest);
-
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
-
-  if (error) {
-    return <span>Some error ocurred</span>;
-  }
-
-  return <span>{data?.message}</span>;
-}
-
-function App(): ReactElement {
+function App() {
   return (
     <div className="App">
       <MenuPage />
