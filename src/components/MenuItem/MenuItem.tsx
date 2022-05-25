89b5f2a9f633381b47ff494b10c0fb02adeeefd4
Patryk Jurczyk <jurczykpatryk8@gmail.com>
2022-05-25 11:37:19 +0200

    Add counter

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,38 +1,39 @@
 import { Box, Button, Modal, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
-import styles from "./MenuItem.module.css";
+
+import styles from './MenuItem.module.css';
 
 const useMenuItem = () => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const inc = () => {};
-  const dec = () => {};
+  // const inc = () => {};
+  // const dec = () => {};
 
   return {
     open,
     handleOpen,
     handleClose,
-    inc,
-    dec,
+    // inc,
+    // dec,
   } as const;
 };
 
 export function MenuItem() {
-  const { open, handleOpen, handleClose, inc, dec } = useMenuItem();
-
+  const { open, handleOpen, handleClose } = useMenuItem();
+  const [count, setCount] = useState(0);
   return (
     <Box className={styles.MenuItem}>
       <Typography className={styles.header}>GNOCCHETTI</Typography>
       <Typography className={styles.description}>shrimp, chili, rosemary oil</Typography>
       <Typography className={styles.weight}>100g</Typography>
       <Typography className={styles.price}>$32</Typography>
-      <Button className={styles.decreaseButton} onClick={dec} variant="contained">
+      <Button className={styles.decreaseButton} onClick={() => setCount(count - 1)} variant="contained">
         -
       </Button>
-      <Typography className={styles.amount}>{}</Typography>
-      <Button className={styles.increaseButton} onClick={inc} variant="contained">
+      <Typography className={styles.amount}>{count}</Typography>
+      <Button className={styles.increaseButton} onClick={() => setCount(count + 1)} variant="contained">
         +
       </Button>
       <Button onClick={handleOpen} className={styles.moreInfoButton} variant="contained">
